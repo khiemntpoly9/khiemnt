52d4c7e3cf6fe057519fc863f4be2e9bb39736c7
Trung Khiem <trungkhiem1412@gmail.com>
2024-09-07 16:36:26 +0700

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+	import { useDark, useToggle } from '@vueuse/core';
+	import HelloWorld from './components/HelloWorld.vue';
+
+	const isDark = useDark();
+	const toggleDark = useToggle(isDark);
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<div>isDark: {{ isDark }}</div>
+	<button @click="toggleDark()" class="px-4 py-2 bg-green-500 rounded dark:bg-purple-500">Dark Mode</button>
+	<div>
+		<a href="https://vitejs.dev" target="_blank">
+			<img src="/vite.svg" class="logo" alt="Vite logo" />
+		</a>
+		<a href="https://vuejs.org/" target="_blank">
+			<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+		</a>
+	</div>
+	<HelloWorld msg="Vite + Vue" />
+	<h1>{{ $t('welcome') }}</h1>
+	<p>{{ $t('description') }}</p>
+
+	<div class="locale-changer">
+		<select v-model="$i18n.locale">
+			<option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">
+				{{ locale }}
+			</option>
+		</select>
+	</div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+	.logo {
+		height: 6em;
+		padding: 1.5em;
+		will-change: filter;
+		transition: filter 300ms;
+	}
+	.logo:hover {
+		filter: drop-shadow(0 0 2em #646cffaa);
+	}
+	.logo.vue:hover {
+		filter: drop-shadow(0 0 2em #42b883aa);
+	}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import './style.css'
+import en from './lang/en.json'
+import vi from './lang/vi.json'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const i18n = createI18n({
+  // something vue-i18n options here ...
+  locale: 'vi',
+  messages: {
+    en: en,
+    vi: vi
+  }
+})
+
+
+
+createApp(App).use(i18n).mount('#app')
